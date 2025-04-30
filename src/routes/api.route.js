886dd460aff3aaed3eb13099a2f4288dd8f2ec7f
salmanzego <salmaannofficial@gmail.com>
2025-05-01 05:29:37 +0530

    fix: resolve merge conflict and restore device addition route

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -1,13 +1,8 @@
 const express = require('express');
-<<<<<<< HEAD
-const router = express.Router();    
-
-=======
 const router = express.Router();
 const { PlugControll } = require('../controllers/api.controller');
 const { verifyApiKey }= require('../middlewares/apikeyauth.middleware');
 
 router.post('/control/plug',verifyApiKey, PlugControll);
->>>>>>> 86be83b59638220cf2856e5ad5b837a689f85f72
 
 module.exports = router;
--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { protect } = require('../middlewares/apikeyauth.middleware');
 const { addDevice } = require('../controllers/device.controller');
 
-// router.post('/add', protect, addDevice);
+router.post('/add', protect, addDevice);
 
 module.exports = router;
