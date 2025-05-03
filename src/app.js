3444875edb2b45e3f13823f3af3b3445fe7d3f77
Muhammad Althaf P A <althafmuhammad2115@gmail.com>
2025-05-03 17:22:09 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth.route');
 const deviceRoutes = require('./routes/device.route');
 const apiRoutes = require('./routes/api.route');
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/user/device', deviceRoutes);
